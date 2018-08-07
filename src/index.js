340c7862c6bf8e18a55d97cbaf72cf23d9c3e437
codaboba <iamphanle@gmail.com>
2018-08-07 15:06:16 -0500

    added mapbox

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('IN INDEX.JS');
+const mapboxgl = require('mapbox-gl');
+mapboxgl.accessToken =
+  'pk.eyJ1IjoicGhhbm55Y29kZXMiLCJhIjoiY2prazRwcHM1MDNqbzN2cGw3MGRsaXF1ZiJ9.6pYqWruzJiNU_aU9mu8dYA';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-87.6354, 41.8885],
+  zoom: 12, // starting zoom
+  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
+});
